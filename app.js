04ce8b5c02b02813d9b11a0f753db7f075a734d4
otomdee <otomdaniel65@gmail.com>
2024-11-29 09:14:55 +0100

    add ejs templates

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const authorRouter = require("./routes/authorRoutes.js");
 const indexRouter = require("./routes/indexRoutes.js");
+const path = require("node:path");
 
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,8 +2,15 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
+const links = [
+    { href: "/", text: "Home" },
+    { href: "about", text: "About" },
+  ];
+
+const users = ["Rose", "Cake", "Biff"];
+
 indexRouter.get('/', (req, res) => {
-    res.send("Index page");
+    res.render("index", { message: "EJS rocks!", links : links, users: users });
 })
 
 module.exports = indexRouter;
